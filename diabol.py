42d6297f2ed6c42e9f75c6b29ed7fe98b5ce6f25
Mario Salazar <93163321+msalaztor@users.noreply.github.com>
2023-02-15 22:33:48 -0400

    Update diabol.py

--- diabol.py
+++ diabol.py
@@ -4,7 +4,6 @@
 
 # Estas son las librerias que se han utilizado para desarrollar la aplicación
 import pandas as pd
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
